882eecbcd3f877d9346419e2bf80a9b2ca398f69
Harshitha <moolya.harshitha18@siesgst.ac.in>
2021-05-14 16:29:59 +0530

    Update add.c

--- add.c
+++ add.c
@@ -2,10 +2,12 @@
 #include<conio.h>
 void main()
 {
-int a,b,c;
+int a,b,c,d;
 printf("Enter values for a and b");
 scanf("%d %d",&a,&b);
 c=a+b;
 printf("\nAddition is %d",c);
+d=a-b;
+printf("\nSubtraction is %d",d);
 getch();
 }
